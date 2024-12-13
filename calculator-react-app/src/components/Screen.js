f95c038342310b350708d576483e6560767a6727
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2024-12-13 19:51:42 +0100

    reinstalld react, fixed bug with textfit

--- calculator-react-app/src/components/Screen.js
+++ calculator-react-app/src/components/Screen.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Textfit } from 'react-textfit';
+import { Textfit } from "react-textfit";
 import './Screen.css';
 
 const Screen = ({ value }) => {
     return (
-        <Textfit className="screen" mode="single" max={70}>
-            {value}
-        </Textfit>
+      <Textfit className='screen' mode='single' max={70}>
+        {value}
+      </Textfit>
     );
-};
+  };
 
 export default Screen;
